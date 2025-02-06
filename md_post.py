c718f1a2b5af646fe5bf442d63331abf8d3004c8
Alan Mitchell <tabb99@gmail.com>
2025-02-06 14:41:18 -0900

    Moved to local DuckDB database.

--- md_post.py
+++ md_post.py
@@ -5,11 +5,28 @@ from pathlib import Path
 import duckdb
 import pandas as pd
 
-import settings
 import things_parser
 
-# Connect to MotherDuck (Use your own token)
-conn = duckdb.connect("md:things", config={"motherduck_token": settings.MD_ACCESS_TOKEN})
+db_path = Path(__file__).parent / "db" / "things.db"
+conn = duckdb.connect(db_path)
+
+# Check to see if the gateway table exists
+tables = conn.execute("SHOW TABLES").fetchdf()
+if "gateway" not in tables["name"].values:
+    # create the gateway table
+    conn.execute("""
+CREATE TABLE gateway (
+  source VARCHAR,
+  ts TIMESTAMP,
+  device_id VARCHAR,
+  device_eui VARCHAR,
+  frame_counter INTEGER,
+  gateway_id VARCHAR,
+  gateway_eui VARCHAR,
+  signal_snr FLOAT,
+  signal_rssi INT2,
+  data_rate VARCHAR
+)""")
 
 column_names = things_parser.uplink_gateway_columns()
 log_dir = Path(__file__).parent / "gtw-log"
@@ -21,7 +38,7 @@ for fpath in log_dir.glob("gtw.log.*"):
         df["ts"] = pd.to_datetime(df["ts"], unit="s")
         df.to_parquet(log_dir / 'temp.parquet')
         conn.execute(f'''
-            COPY gtw 
+            COPY gateway
             FROM '{parquet_path}'
             (FORMAT 'parquet');
             ''')
--- settings_example.py
+++ settings_example.py
@@ -12,12 +12,8 @@ API_KEYS = {
 
 # Number of minutes for log file rotation, which then causes the
 # rotated log file to be posted to the MotherDuck database.
-UPLINK_LOG_FILE_ROTATION_TIME = 10
+UPLINK_LOG_FILE_ROTATION_TIME = 5
 
 # Number of log file backups to keep
-UPLINK_LOG_FILE_BACKUP_COUNT = 5
+UPLINK_LOG_FILE_BACKUP_COUNT = 12
 
-# ----------  Mother Duck Database Info
-
-# Access token
-MD_ACCESS_TOKEN = "Access Token goes Here"
